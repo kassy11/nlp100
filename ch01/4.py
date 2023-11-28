78ab70f4d60c0a900843d558817b06640e2230d2
Kotaro Kashihara <kotaro11kassy@gmail.com>
2023-11-28 13:52:39 +0900

    fix ch01/4

--- ch01/4.py
+++ ch01/4.py
@@ -1,15 +1,11 @@
-sentence = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
-
-numbers = [1, 5, 6, 7, 8, 9, 15, 16, 19]
-words = sentence.split(" ")
-
-# TODO: 問題文の意味が良くわからない
-result_dict = {}
-for i in range(len(words)):
-    if i + 1 in numbers:
-        result_dict[words[i]] = i + 1
+str = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+splits = str.split()
+one_ch = [1, 5, 6, 7, 8, 9, 15, 16, 19]  # 1文字を取り出す単語の番号リスト
+ans = {}
+for i, word in enumerate(splits):
+    if i + 1 in one_ch:
+        ans[word[:1]] = i + 1  # リストにあれば1文字を取得
     else:
-        result_dict[words[i]] = i + 1
+        ans[word[:2]] = i + 1  # なければ2文字を取得
 
-
-print(result_dict)
+print(ans)
